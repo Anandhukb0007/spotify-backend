0b878522da2a28c4af9debb9759c66b7cb8ebe80
Anandhukb0007 <anandhukb0007@gmail.com>
2024-07-04 16:04:17 +0530

    updated backend

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const JwtStrategy = require("passport-jwt").Strategy,
+    ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    `mongodb+srv://Spotifyadmin:${process.env.MONGO_PASSWORD}@cluster0.lovlzcj.mongodb.net/`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch((err) => {
+    console.error("Error while connecting to Mongo:", err);
+  });
+
+// Setup passport-jwt
+
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "thisKeyIsSupposedToBeSecret";
+passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+        User.findOne({_id: jwt_payload.identifier}, function (err, user) {
+            // done(error, doesTheUserExist)
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        });
+    })
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/auth", authRoutes);
+app.use("/song", songRoutes);
+app.use("/playlist", playlistRoutes);
+
+// Start the server
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
